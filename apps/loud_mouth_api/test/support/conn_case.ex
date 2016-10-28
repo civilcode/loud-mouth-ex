b66695b98f366d5f78de34327ce5d1d3ddab5fcc
frahugo / nicholasjhenry <hugo@cekoya.com , nicholas.henry@gmail.com>
2016-10-28 11:49:24 -0400

    Run tests asynchronously

--- apps/loud_mouth_api/test/support/conn_case.ex
+++ apps/loud_mouth_api/test/support/conn_case.ex
@@ -32,8 +32,13 @@ defmodule LoudMouthAPI.ConnCase do
     end
   end
 
-  setup _tags do
+  setup tags do
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(LoudMouth.Repo)
+
+    unless tags[:async] do
+      Ecto.Adapters.SQL.Sandbox.mode(LoudMouth.Repo, {:shared, self()})
+    end
+
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
 end
